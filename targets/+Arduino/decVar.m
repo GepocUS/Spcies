08cab2fdf400a7712b1d8436b8007dda10dfb825
Pablo Krupa <pkrupa@us.es>
2020-09-03 23:33:16 +0200

    Added documentation in /+Arduino

--- targets/+Arduino/decVar.m
+++ targets/+Arduino/decVar.m
@@ -1,72 +1,97 @@
-%% decVar function for Unity
+%% decVar - Arduino version
+% Writes the code for declaring a variable in Arduino
+%
+% INPUTS: 
+%   - var: variable to be declared. Single-rowed cell with 5 columns.
+%          Each column contains the following information:
+%          1: name. String containing the name of the variable.
+%          2: value. Variable to be declared.
+%          3: initialize. Boolean that indicates if the variable is
+%             initialized to the variable given in the field "value".
+%          4: type. String defining the type of variable. Can be any of the following:
+%                   - float, double, bool, int, uint, udint, dint, usint
+%          5: class. String defining the class of variable. Can be any of the following:
+%                   - define: a C #define
+%                   - constant: sets the variable as a C constant
+%                   - variable: a normal C variable
+% 
+% OUTPUTS:
+%   - s: String containing the text for the variable declaration.
+%
 
-function s = decVar(var, position)
-if nargin < 2
-    position = [];
-end
+% Function bellonging to the Spcies toolbox
+% Author: Pablo Krupa, pkrupa@us.es, (c) 2020
+%
 
-%% Preliminaries
-s = [''];
-name = var{1};
-value = var{2};
-initialize = var{3};
-type = var{4};
-class = var{5};
+% Devlog and notes:
+%   - 20/09/03: Cleaned code and added documentation
 
-% Detect order of variable
-dim = size(value);
-if max(dim) == 1
-    order = 'scalar';
-elseif max(dim) == 0
-    order = 'other';
-else
-    if length(dim) == 2
-        if min(dim) == 1
-            order = 'vector';
-        else
-            order = 'matrix';
+function s = decVar(var)
+
+    %% Initialize and rename the fields of "var"
+    s = [''];
+    name = var{1};
+    value = var{2};
+    initialize = var{3};
+    type = var{4};
+    class = var{5};
+    
+    % Detect order of the variable: it can be a scalar, a vector, a matrix or a 3D matrix
+    dim = size(value);
+    if max(dim) == 1 % Scalar
+        order = 'scalar';
+
+    elseif max(dim) == 0 % Something non-dimensional
+        order = 'other';
+
+    else
+
+        if length(dim) == 2 % Vector o multidimensional matrix (2D or 3D)
+
+            if min(dim) == 1
+                order = 'vector';
+            else
+                order = 'matrix';
+            end
+
+        elseif length(dim) == 3
+            order = '3Dmatrix';
+
+        else 
+            error(['Size of variable ' name ' is not compatible']);
         end
-    elseif length(dim) == 3
-        order = '3Dmatrix';
-    else 
-        error(['Size of variable ' name ' is not compatible']);
+
     end
-end
-
-% Get type
-if strcmp(type, 'float')
-    type = 'float';
-elseif strcmp(type, 'double')
-    type = 'double';
-elseif strcmp(type, 'time')
-    type = 'TIME';
-elseif strcmp(type, 'bool')
-    type = 'boolean';
-elseif strcmp(type, 'int')
-    type = 'int';
-elseif strcmp(type, 'uint')
-    type = 'unsigned int';
-elseif strcmp(type, 'udint')
-    type = 'unsigned long';
-elseif strcmp(type, 'dint')
-    type = 'long';
-elseif strcmp(type, 'usint')
-    type = 'unsigned long';
-else
-    %error('decVar_Unity:VarType', ['Variable type (' type ') not recognized for variable ' name]);
-    initialize = 0;
-end
-
-%% Create string for variable declaration
-
-% If it is a deffine
-if strcmp(class, 'define')
     
-    s = [s sprintf('#define %s ', name) writeValue(value, type) '\n'];
-  
-else
+    % Get type
+    if strcmp(type, 'float')
+        type = 'float';
+    elseif strcmp(type, 'double')
+        type = 'double';
+    elseif strcmp(type, 'bool')
+        type = 'boolean';
+    elseif strcmp(type, 'int')
+        type = 'int';
+    elseif strcmp(type, 'uint')
+        type = 'unsigned int';
+    elseif strcmp(type, 'udint')
+        type = 'unsigned long';
+    elseif strcmp(type, 'dint')
+        type = 'long';
+    elseif strcmp(type, 'usint')
+        type = 'unsigned long';
+    else
+        initialize = 0;
+    end
+
+    %% Create string for variable declaration
     
-    % Set constant status
+    % Add the #define tag if the class is set to 'define'
+    if strcmp(class, 'define')
+        s = [s sprintf('#define %s ', name) writeValue(value, type) '\n'];
+    else
+    
+    % Add the const tag if the class is set to 'constant'
     if strcmp(class, 'constant')
         s = [s sprintf('const ')];
     end
@@ -85,8 +110,12 @@ else
     
     if initialize
         s = [s sprintf(' = ')];
+        
+        % Scalar
         if strcmp(order, 'scalar')
             s = [s writeValue(value, type)];
+
+        % Vector
         elseif strcmp(order, 'vector')
             s = [s sprintf('{ ')];
             write_comma = false;
@@ -99,6 +128,8 @@ else
                 s = [s writeValue(value(i), type)];
             end
             s = [s sprintf(' }')];
+
+        % Matrix
         elseif strcmp(order, 'matrix')
             s = [s sprintf('{ ')];
             for i = 1:dim(1)
@@ -118,6 +149,8 @@ else
                 end
             end
             s = [s sprintf(' }')];
+
+        % 3D Matrix
         elseif strcmp(order, '3Dmatrix')
             s = [s sprintf('{ ')];
             for k = 1:dim(3)
@@ -150,24 +183,27 @@ else
     
     s = [s sprintf(';\n')];
 
-end
+    end
 
 end
+
+%% Auxiliary function that writes the value of the variable according to its type
 
 function s = writeValue(value, type)
     
     if contains(type, 'int')
         s = sprintf('%d', value);
+
     elseif contains(type, 'Bool')
         if value == true
             s = sprintf('true');
         else
             s = sprintf('false');
         end
-    elseif contains(type, 'TIME')
-        s = sprintf('%d', value);
+
     else
         s = sprintf('%1.10f', value);
     end
     
 end
+
--- targets/+Arduino/declareVariables.m
+++ targets/+Arduino/declareVariables.m
@@ -1,13 +1,35 @@
-%% Function that declares the variables for Arduino
+%% declareVariables - Arduino version
+% Function that declares the variables for Arduino
+% 
+% INPUTS:
+%   - vars: cell containing the information of each variable in each row.
+%           Each row contains the following information:
+%           vars = (name, value, initialize, type, class)
+%           For a description of each field see the function +Arduino/decVar.m
+%           Arduino does not require any additional steps before calling the decVar function.
+%
+% OUTPUTS:
+%   - s: String containing the text for the variable declaration in the Arduino environment.
+%
+
+% Function bellonging to the Spcies toolbox
+% Author: Pablo Krupa, pkrupa@us.es, (c) 2020
+%
+
+% Devlog and notes:
+%   - 20/09/03: Cleaned code and added documentation
 
 function s = declareVariables(vars)
-import Arduino.decVar;
-%% Preliminaries
-s = ['']; % String array containing the text for variable declaration
-numVars = size(vars, 1);
+    import Arduino.decVar;
 
-for i = 1:numVars
-    s = [s decVar(vars(i,:))];
+    %% Preliminaries
+    s = ['']; % Initialize s to an empty string
+    numVars = size(vars, 1); % Number of variables stored in vars (number of rows)
+
+    % Generate string s
+    for i = 1:numVars
+        s = [s decVar(vars(i,:))];
+    end
+
 end
 
-end
