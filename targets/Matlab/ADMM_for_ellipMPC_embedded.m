9ed3e13711ae969dc9e5187ed36ffcd18978c7c9
Pablo Krupa <pkrupa@us.es>
2020-12-19 19:22:35 +0100

    Matlab version of ADMM_ellipMPC solver is up to date with the C version

--- targets/Matlab/ADMM_for_ellipMPC_embedded.m
+++ targets/Matlab/ADMM_for_ellipMPC_embedded.m
@@ -1,16 +1,18 @@
 %% This is the sparse version of the RMPC solver
 
-function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, varargin)
+function [v_0, k, e_flag, Hist] = ADMM_for_ellipMPC_embedded(var, x0, xr, ur, varargin)
     timer_function = tic;
     
     %% Default values
-    def_z_0 = zeros(var.N*(var.n+var.m), 1);
-    def_lambda_0 = zeros(var.N*(var.n+var.m), 1);
-    def_k_max = 1000; % Maximum number of iterations
-    def_tol = 1e-3; % Exit tolerance
+    def_v_0 = zeros(var.N*(var.n+var.m), 1); % Default initial value for the decision variables v
+    def_lambda_0 = zeros(var.N*(var.n+var.m), 1); % Default initial value for the dual variables lamnda
     def_genHist = 0; % Default amount of data generated for Hist
     def_verbose = 1; % Default amount of information displayed: Displayes end of algorithm info only
-    
+    def_k_max = 1000; % Maximum number of iterations
+    def_tol = 1e-3; % Exit tolerance
+    def_in_engineering = false; % Default value of the flag that determined if the input variables are given in engineering units
+    def_options = struct('tol', def_tol, 'k_max', def_k_max, 'in_engineering', def_in_engineering);
+                     
     %% Parser
     par = inputParser;
     par.CaseSensitive = false;
@@ -18,89 +20,99 @@ function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, vararg
     % Required
     addRequired(par, 'var', @(x) isstruct(x));
     addRequired(par, 'x0', @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
+    addRequired(par, 'xr', @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
+    addRequired(par, 'ur', @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
     % Optional
-    addOptional(par, 'z_0', def_z_0, @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
+    addOptional(par, 'v_0', def_v_0, @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
     addOptional(par, 'lambda_0', def_lambda_0, @(x) isnumeric(x) && (min(size(x))==1) || isempty(x));
     % Name-value parameters
-    addParameter(par, 'k_max', def_k_max, @(x) isnumeric(x) && (x>0) && x==floor(x));
-    addParameter(par, 'tol', def_tol, @(x) isnumeric(x) && (x>=0));
     addParameter(par, 'genHist', def_genHist, @(x) isnumeric(x) && (x>=0));
     addParameter(par, 'verbose', def_verbose, @(x) isnumeric(x) && (x>=0));
+    addParameter(par, 'options', def_options, @(x) isstruct(x) || isempty(x));
+    
     % Parse
-    parse(par, var, x0, varargin{:});
+    parse(par, var, x0, xr, ur, varargin{:});
     % Rename
     var = par.Results.var;
     x0 = par.Results.x0;
-    z = par.Results.z_0;
+    xr = par.Results.xr;
+    ur = par.Results.ur;
+    v = par.Results.v_0;
     lambda = par.Results.lambda_0;
-    k_max = par.Results.k_max;
-    tol = par.Results.tol;
     genHist = par.Results.genHist;
     verbose = par.Results.verbose;
     % Check arguments
-    if isempty(z); z = def_z_0; end % Default if an empty array is provided
+    if isempty(v); v = def_v_0; end % Default if an empty array is provided
     if isempty(lambda); lambda = def_lambda_0; end % Default if an empty array is provided
-    if size(z, 2)>1; z = z'; end % Make a column vector
+    if size(v, 2)>1; v = v'; end % Make a column vector
     if size(lambda, 2)>1; lambda = lambda'; end % Make a column vector
     if genHist>2; genHist = 2; end
     if verbose>3; verbose = 3; end
     
+    % Set default options if options is empty
+    if isempty(par.Results.options)
+        options = def_options;
+    else
+        options = par.Results.options;
+    end
+    if ~isfield(options, 'tol'); options.tol = def_tol; end
+    if ~isfield(options, 'k_max'); options.k_max = def_k_max; end
+    if ~isfield(options, 'in_engineering'); options.in_engineering = def_in_engineering; end
+    
     %% Variable declaration
     done = false; % Flag that indicates the end of the algorithm
     k = 0; % Counter for the number of iterations
-    
-    % Times
-    time_z = zeros(1, k_max);
-    time_z_hat = zeros(1, k_max);
-    time_lambda = zeros(1, k_max);
-    time_exit = zeros(1, k_max);
-    time_iter = zeros(1, k_max);
     
     % Dimensions
     n = var.n;
     m = var.m;
     N = var.N;
     
-    % Variables
-    
-    z_0 = z(1:m);
-    z_N = z(end-n+1:end);
-    z = reshape(z(m+1:end-n), n+m, N-1); % Decision variable z1 in matrix form
+    % Decision variables
+    z_0 = zeros(m, 1);
+    z_N = zeros(n, 1);
+    z = zeros(n+m, N-1);
     
     z1_0 = zeros(m, 1);
     z1_N = zeros(n, 1);
-    z1 = zeros(n, 1); % Decision variable z1 in matrix form
-    
-    q_hat_0 = zeros(m, 1);
-    q_hat_N = zeros(n, 1);
-    q_hat = zeros(n, 1);
-    
-    v_0 = zeros(m, 1);
-    v_N = zeros(n, 1);
-    v = zeros(n+m, N-1);
-    aux_N = zeros(n, 1);
-    project_me = zeros(n, 1);
+    z1 = zeros(n, 1);
+
+    v_0 = v(1:m);
+    v_N = v(end-n+1:end);
+    v = reshape(v(m+1:end-n), n+m, N-1);
     
     lambda_0 = lambda(1:m);
     lambda_N = lambda(end-n+1:end);
     lambda = reshape(lambda(m+1:end-n), n+m, N-1); % Decision variable lambda in matrix form
-
+    
+    % Other variables
+    aux_N = zeros(n, 1);
     mu = zeros(n, N);
     
     rho_0 = var.rho_0;
     rho = var.rho;
     rho_N = var.rho_N;
     
-    u_opt = zeros(m, 1); % Control action to be applied
-    res = zeros(n+m, N+1); % Residual
-    res_1 = 0; % One norm of the residual
-
     AB = var.AB;
     Hi = var.Hi;
     Hi_0 = var.Hi_0;
     Hi_N = var.Hi_N;
     P_half = var.P_half;
     P = var.P;
+    
+    %% Obtain variables in scaled units
+    timer_iter = tic;
+    
+    if options.in_engineering
+        for j = 1:n
+            x0(j) = var.scaling_x(j)*( x0(j) - var.OpPoint_x(j) );
+            xr(j) = var.scaling_x(j)*( xr(j) - var.OpPoint_x(j) );
+        end
+        for j = 1:m
+            ur(j) = var.scaling_x(j)*( ur(j) - var.OpPoint_x(j) );
+        end
+    end
+            
     
     % Update b with the current state x0
     for j = 1:n
@@ -124,7 +136,6 @@ function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, vararg
     
     %% Iterations
     while ~done
-        timer_iter = tic;
         k = k + 1;
         
         %% Step 0: Save value of z
@@ -330,7 +341,8 @@ function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, vararg
             vPv = vPv + (v_N(j) - var.c(j))*aux_N(j);
         end
         
-        if vPv > var.r^2 % If v_N belongs to the ellipsoid, I don't need to do anything else
+        % If v_N does not belong to the ellipsoid I need to perform the projection step
+        if vPv > var.r^2
             vPv = var.r/sqrt(vPv);
             for j = 1:n
                 v_N(j) = vPv*(v_N(j) - var.c(j)) + var.c(j);
@@ -361,36 +373,54 @@ function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, vararg
 
         %% Step 4: Compute residual
         
-        % Compute the residual vector
-            % Compute the first n elements
+        res_flag = false; % Flag used to determine if the residuals are satisfied
+        res_fixed_point = 0; % Residual for fixed point
+        res_primal_feas = 0; % Residual for primal feasibility
+        
+        % Computer the first n elements
         for j = 1:m
-            res(j, 1) = z1_0(j) - z_0(j);
-        end
-            % Compute all others except for the last n
-        for l = 1:N-1
-            for j = 1:n+m
-                res(j, l+1) = z1(j, l) - z(j, l);
-            end
-        end
-            % Compute the last n
-        for j = 1:n
-            res(j, N+1) =  z1_N(j) - z_N(j);
-        end
-        
-        % Compute the 1-norm of the residual vector
-        res_1 = 0;
-        for l = 1:N+1
-            for j = 1:n+m
-                res_1 = max( res_1, abs(res(j, l)));
+            res_fixed_point = abs(z1_0(j) - z_0(j));
+            res_primal_feas = abs(z_0(j) - v_0(j));
+            if res_fixed_point > options.tol || res_primal_feas > options.tol
+                res_flag = 1;
+                break;
             end
         end
         
+        % Compute the last n elements
+        if ~res_flag
+            for j = 1:n
+                res_fixed_point = abs(z1_N(j) - z_N(j));
+                res_primal_feas = abs(z_N(j) - v_N(j));
+                if res_fixed_point > options.tol || res_primal_feas > options.tol
+                    res_flag = 1;
+                    break;
+                end
+            end
+        end
+        
+        % Compute all the other elements
+        if ~res_flag
+            for l = 1:N-1
+                for j = 1:n+m 
+                    res_fixed_point = abs(z1(j, l) - z(j, l));
+                    res_primal_feas = abs(z(j, l) - v(j, l));
+                    if res_fixed_point > options.tol || res_primal_feas > options.tol
+                        res_flag = 1;
+                        break;
+                    end
+                end
+                if res_flag
+                    break;
+                end
+            end
+        end
+                
         %% Step 5: Exit condition
-        
-        if res_1 <= tol
+        if ~res_flag
             done = true;
             e_flag = 1;
-        elseif k >= k_max
+        elseif k >= options.k_max
             done = true;
             e_flag = -1;
         end
@@ -400,10 +430,9 @@ function [v_0, k, e_flag, vars] = ADMM_for_RMPC_embedded(var, x0, xr, ur, vararg
     %% Extract variables
     time = toc(timer_function);
     
-    vars.z = [z_0; z(:); z_N];
-    vars.v = [v_0; v(:); v_N];
-    vars.lambda = [lambda_0; lambda(:); lambda_N];
-    vars.res = res(:);
-    vars.time = time;
+    Hist.z = [z_0; z(:); z_N];
+    Hist.v = [v_0; v(:); v_N];
+    Hist.lambda = [lambda_0; lambda(:); lambda_N];
+    Hist.time = time;
 
 end
