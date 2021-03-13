b72521027938d893f03465c6bf71b271a9754fbe
Pablo Krupa <pkrupa@us.es>
2021-03-13 17:23:10 +0100

    Added functions for computing sparse matrix representations

---
+++ +utils/full2CSC.m
@@ -0,0 +1,42 @@
+%% full2CSC - Compute Compressed Sparse Column form of a sparse matrix
+% 
+% M_spare = full2CSC(M) computes the Compressed Sparse Column (CSC)
+% representation of the (sparse) matrix M.
+% 
+% INPUTS:
+%   - M: Matrix to be converted
+%
+% OUTPUS:
+%   - M_sparse: Structure containing the sparse representation of M
+%       - val: Vector containing the values of the non-zero elements
+%       - row: Vector containing the row-indexes of each of the
+%              non-zero elements
+%       - col: Vector containing the column-index range of the
+%              non-zero elements
+%       - nnz: Number of non-zero elements
+%       - nrow: number of rows of M
+%       - ncol: number of columns of M
+%
+% This function is part of Spcies: https://github.com/GepocUS/Spcies
+% 
+
+function M_sparse = full2CSC(M, threshhold)
+    if nargin == 1
+        threshhold = 0; % Any number smaller than threshhold is considered a 0
+    end
+    
+	%% Compute dimensions of M
+    [n, m] = size(M);
+    
+    %% Compute the CSR representation of the transposed
+    M_CSR = utils.full2CSR(M', threshhold);
+    
+    %% Fix the values of nrow and ncol
+    M_sparse.val = M_CSR.val;
+    M_sparse.row = M_CSR.col;
+    M_sparse.col = M_CSR.row;
+    M_sparse.nnz= M_CSR.nnz;
+    M_sparse.nrow = n;
+    M_sparse.ncol = m;
+    
+end
