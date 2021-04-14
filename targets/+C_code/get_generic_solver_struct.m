f9d2975a4c337db9ec569d5bbb6f6817bb23afb3
Pablo Krupa <pkrupa@us.es>
2021-04-14 23:11:47 +0200

    Documented get_generic_solver_struct

--- targets/+C_code/get_generic_solver_struct.m
+++ targets/+C_code/get_generic_solver_struct.m
@@ -1,10 +1,20 @@
-
+%% get_generic_solver_struct
+% 
+% This function returns the text of the generic structure
+% of the code for solvers in C.
+%
+% The text is loaded from the file ./generic_solver_struct.c
+%
+% This function is part of Spcies: https://github.com/GepocUS/Spcies
+% 
 
 function struct_text = get_generic_solver_struct()
 
+    % Get the path to the directory where this function is saved
     full_path = mfilename('fullpath');
     this_path = fileparts(full_path);
     
+    % Read the file ./generic_solver_struct.c
     struct_text = fileread([this_path '/generic_solver_struct.c']);
 
 end
