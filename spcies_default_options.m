1cd0582cd8584bb344c154078ca89bb7dc81e7fe
Pablo Krupa <pkrupa@us.es>
2021-04-19 18:22:26 +0200

    Cleanup spcies_default_options

--- spcies_default_options.m
+++ spcies_default_options.m
@@ -1,11 +1,15 @@
-%% Spcies_default_options - Returns the default options of the Spcies toolbox
+%% spcies_default_options - Returns the default options of the Spcies toolbox
 % 
 % This function containts the default options of the Spcies toolbox.
 % It returns them in a structure that can be used as the argument
 % spcies_options of other functions of the toolbox.
 % 
 % The options of the toolbox are:
-%   - target: String that determines the target embedded system.
+%   - type: String that determines the type of controller to be generated.
+%   - platform: String that determines the target embedded system or platform.
+%   - method: String that determines the optimization method used.
+%   - subclass: String that determines the exact solver to be used.
+%               (only used if various are available).
 %   - save_name: String that indicates the name with which files are saved.
 %                If empty, then each controller will use its own
 %                default naming convention.
@@ -17,10 +21,15 @@
 %               This penalty is usually a scalar, but this boolean
 %               option forces the use of an array if set to true.
 %
+% OUTPUTS:
+%   - def_opt: Structure containing the default options of the toolbox.
+%   - def_metod: Structure containing the default method of eath type.
+%   - def_subclass: Structure containing the default subclass of each type.
+%
 % This function is part of Spcies: https://github.com/GepocUS/Spcies
 % 
 
-function [def_opt, def_method, def_subclass] = Spcies_default_options()
+function [def_opt, def_method, def_subclass] = spcies_default_options()
     
     % Default options of the toolbox
     def_opt.method = ''; % Default method
@@ -31,16 +40,16 @@ function [def_opt, def_method, def_subclass] = Spcies_default_options()
     def_opt.directory = '$SPCIES$'; % Default directory where to save files
     def_opt.override = true; % Determined if files are overwritten if one with the same name already exists
     def_opt.force_vector_rho = false; % If true, forces the penalty parameter rho to be defined as a vector
-    def_opt.save = 1; % Determines if the file is saved
+    def_opt.save = 1; % Determines if the file is saved (currently unused)
     
-    % Default metods for each type
+    % Default metods for each type of controller
     def_method.laxMPC = 'ADMM';
     def_method.equMPC = 'ADMM';
     def_method.ellipMPC = 'ADMM';
     def_method.MPCT = 'EADMM';
     def_method.Other = '';
     
-    % Default subclasses for each type
+    % Default subclasses for each type of controller
     def_subclass.laxMPC = '';
     def_subclass.equMPC = '';
     def_subclass.ellipMPC = '';
