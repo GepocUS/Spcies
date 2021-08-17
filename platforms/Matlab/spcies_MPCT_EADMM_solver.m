e534a3a6dbe0a3b863e8740c4ae17f20746e3f4f
Pablo Krupa <pkrupa@us.es>
2021-08-17 13:39:23 +0200

    Added historics to spcies_MPCT_EADMM_solver

--- platforms/Matlab/spcies_MPCT_EADMM_solver.m
+++ platforms/Matlab/spcies_MPCT_EADMM_solver.m
@@ -49,6 +49,10 @@
 %              - .k_max: Maximum number of iterations of the solver. Defaults to 1000.
 %              - .in_engineering: Boolean that determines if the arguments of the solver are given in
 %                                 engineering units (true) or incremental ones (false - default).
+%   - verbose: Controlls the amount of information printed in the console.
+%              Integer from 0 (no information printed) to 3 (print all information).
+%   - genHist: Controlls the amount of information saved and returned in the output Hist.
+%              Integer from 0 (save minimum amount of data) to 2 (save all data).
 % 
 % OUTPUTS:
 %   - u: Control action to be applied to the system.
@@ -56,34 +60,35 @@
 %   - e_flag: Exit flag of the algorithm.
 %       - 1: Optimal solution found.
 %       - -1: Maximum iterations reaches. Returns last iterate.
-%   - sol: Structure containing the optimal solution of the decision variables and dual variables.
-%       - .z1: Optimal decision variables z1.
-%       - .z2: Optimal decision variables z2.
-%       - .z3: Optimal decision variables z3.
-%       - .lambda: Optimal dual variables.
-%       - .res: Value of the residual.
+%   - Hist: Structure containing information of the iterations and final outcome of the solver
+%       - sol: Structure containing the returned solution of the primal and dual variables.
+%           - .z1: Returned decision variables z1.
+%           - .z2: Returned decision variables z2.
+%           - .z3: Returned decision variables z3.
+%           - .lambda: Returned dual variables.
+%           - .res: Value of the residual.
+%       - res: Structure containing information of the values of the residuals of the returned solution.
+%       - hNRes_pf: Historic of the infinity norm of the residual of primal feasibility (only saved in genHist > 0).
+%       - hNRes_z2: Historic of the infinity norm of the residual of fixed point for z2 (only saved in genHist > 0).
+%       - hNRes_z3: Historic of the infinity norm of the residual of fixed point for z3 (only saved in genHist > 0).
+%       - hZ1: Historic of z1 (only saved in genHist > 1).
+%       - hZ2: Historic of z2 (only saved in genHist > 1).
+%       - hZ3: Historic of z3 (only saved in genHist > 1).
+%       - hLambda: Historic of lambda (only saved in genHist > 1).
 %
 % This function is part of Spcies: https://github.com/GepocUS/Spcies
 % 
 
-function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
+function [u, k, e_flag, Hist] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
     import MPCT.compute_MPCT_EADMM_ingredients
     
     %% Default values
     def_sys = []; % Default value for the sys argument
     def_param = []; % Default value for the param argument
     def_controller = []; % Default value for the controller argument
-    def_rho_base = 3;
-    def_rho_mult = 20;
-    def_epsilon_x = 1e-6;
-    def_epsilon_u = 1e-6;
-    def_inf_bound = 1e6;
-    def_tol = 1e-4;
-    def_k_max = 1000;
-    def_in_engineering = false;
-    def_options = struct('rho_base', def_rho_base, 'rho_mult', def_rho_mult, 'epsilon_x', def_epsilon_x,...
-                         'epsilon_u', def_epsilon_u, 'tol', def_tol, 'k_max', def_k_max,...
-                         'in_engineering', def_in_engineering, 'inf_bound', def_inf_bound);
+    def_genHist = 0; % Default amount of data generated for Hist
+    def_verbose = 1; % Default amount of information displayed
+    def_options = MPCT.def_options_MPCT_EADMM(); % Default values of the options of the solver
     
     %% Parser
     par = inputParser;
@@ -95,6 +100,8 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
     addParameter(par, 'param', def_param, @(x) isstruct(x));
     addParameter(par, 'controller', def_controller, @(x) isa(x, 'ssMPC'));
     addParameter(par, 'options', def_options, @(x) isstruct(x));
+    addParameter(par, 'genHist', def_genHist, @(x) isnumeric(x) && (x>=0));
+    addParameter(par, 'verbose', def_verbose, @(x) isnumeric(x) && (x>=0));
     
     % Parse
     parse(par, varargin{:})
@@ -105,15 +112,8 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
     else
         options = par.Results.options;
     end
-    if ~isfield(options, 'rho_base'); options.rho_base = def_rho_base; end
-    if ~isfield(options, 'rho_mult'); options.rho_mult = def_rho_mult; end
-    if ~isfield(options, 'epsilon_x'); options.epsilon_x = def_epsilon_x; end
-    if ~isfield(options, 'epsilon_u'); options.epsilon_u = def_epsilon_u; end
-    if ~isfield(options, 'inf_bound'); options.inf_bound = def_inf_bound; end
-    if ~isfield(options, 'tol'); options.tol = def_tol; end
-    if ~isfield(options, 'k_max'); options.k_max = def_k_max; end
-    if ~isfield(options, 'in_engineering'); options.in_engineering = def_in_engineering; end
-    
+    options = utils.add_default_options_to_struct(options, def_options);
+
     % Create the controller structure
     if isempty(par.Results.controller)
         controller.sys = par.Results.sys;
@@ -124,6 +124,14 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
             error('Controller object must be of the TrackingMPC class');
         end
     end
+    
+    % Rename and check other arguments
+    genHist = par.Results.genHist;
+    if genHist > 2; genHist = 2; end
+    if genHist < 0; genHist = 0; end
+    verbose = par.Results.verbose;
+    if verbose > 3; verbose = 3; end
+    if verbose < 0; verbose = 0; end
     
     %% Generate ingredients of the solver
     [~, var] = compute_MPCT_EADMM_ingredients(controller, options, []);
@@ -140,8 +148,21 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
     z2 = zeros(m+n, 1);
     z3 = zeros((N+1)*(n+m), 1);
     z2_prev = z2;
-    z3_prec = z3;
+    z3_prev = z3;
     lambda = zeros((N+2)*(n+m) + n, 1);
+    
+    % Historics
+    if genHist > 0
+        hNRes_pf = zeros(1, options.k_max);
+        hNRes_z2 = zeros(1, options.k_max);
+        hNRes_z3 = zeros(1, options.k_max);
+    end
+    if genHist > 1
+        hZ1 = zeros((N+1)*(n+m), options.k_max);
+        hZ2 = zeros(m+n, options.k_max);
+        hZ3 = zeros((N+1)*(n+m), options.k_max);
+        hLambda = zeros((N+2)*(n+m) + n, options.k_max);
+    end
     
     % Obtain x0, xr and ur
     if options.in_engineering
@@ -184,14 +205,20 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
         % Compute z3
         z3 = -var.H3inv*(var.Az3'*mu + q3);
         
-        %% Compute residual
-        res = var.A1*z1 + var.A2*z2 + var.A3*z3 - b;
+        %% Compute residuals
+        res_pf = var.A1*z1 + var.A2*z2 + var.A3*z3 - b; % Residual of primal feasibility
+        res_z2 = z2 - z2_prev; % Residual of fixed point of z2
+        res_z3 = z3 - z3_prev; % Residual of fixed point of z3
+        % Infinity norms of the residuals
+        n_res_pf = norm(res_pf, Inf);
+        n_res_z2 = norm(res_z2, Inf);
+        n_res_z3 = norm(res_z3, Inf);
         
         %% Update lambda
-        lambda = lambda + var.rho.*res;
+        lambda = lambda + var.rho.*res_pf;
         
         %% Exit condition
-        if norm(res, Inf) <= options.tol && norm(z2 - z2_prev, Inf) <= options.tol && norm(z3 - z3_prev, Inf) <= options.tol
+        if n_res_pf <= options.tol && n_res_z2 <= options.tol && n_res_z3 <= options.tol
             done = true;
             e_flag = 1;
         elseif k >= options.k_max
@@ -199,8 +226,21 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
             e_flag = -1;
         end
         
+        %% Update variables and save historics
         z2_prev = z2;
         z3_prev = z3;
+        
+        if genHist > 0
+            hNRes_pf(k) = n_res_pf;
+            hNRes_z2(k) = n_res_z2;
+            hNRes_z3(k) = n_res_z3;
+        end
+        if genHist > 1
+            hZ1(:, k) = z1;
+            hZ2(:, k) = z2;
+            hZ3(:, k) = z3;
+            hLambda(:, k) = lambda;
+        end
         
     end
     
@@ -213,12 +253,30 @@ function [u, k, e_flag, sol] = spcies_MPCT_EADMM_solver(x0, xr, ur, varargin)
         u = z1(n+(1:m));
     end
     
-    % Optimal decision variables
-    sol.z1 = z1;
-    sol.z2 = z2;
-    sol.z3 = z3;
-    sol.lambda = lambda;
-    sol.res = res;
-        
+    % Hist
+        % Optimal decision variables
+    Hist.sol.z1 = z1;
+    Hist.sol.z2 = z2;
+    Hist.sol.z3 = z3;
+    Hist.sol.lambda = lambda;
+    Hist.res.pf = res_pf;
+    Hist.res.z2 = res_z2;
+    Hist.res.z3 = res_z3;
+    Hist.res.norm_f = n_res_pf;
+    Hist.res.norm_z2 = n_res_z2;
+    Hist.res.norm_z3 = n_res_z3;
+    Hist.k = k;
+        % Historics
+    if genHist > 0
+        Hist.hNRes_pf = hNRes_pf(1:k);
+        Hist.hNRes_z2 = hNRes_z2(1:k);
+        Hist.hNRes_z3 = hNRes_z3(1:k);
+    end
+    if genHist > 1
+        Hist.hZ1 = hZ1(:, 1:k);
+        Hist.hZ2 = hZ2(:, 1:k);
+        Hist.hZ3 = hZ3(:, 1:k);
+        Hist.hLambda = hLambda(:, 1:k);
+    end
+    
 end
-
