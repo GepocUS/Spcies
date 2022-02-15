16f68875e533f5ca3275bd7a50d0266ccef00674
Pablo Krupa <pkrupa@us.es>
2022-02-15 19:02:12 +0100

    Bump version

--- spcies.m
+++ spcies.m
@@ -41,7 +41,7 @@ switch varargin{1}
     % 
     case 'version'
         
-        varargout{1} = 'v0.3.6';
+        varargout{1} = 'v0.3.7';
         
         % If git is installed it will return the hash of the current commit
         try
