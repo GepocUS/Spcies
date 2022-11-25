bfbcbb4775b851384e45be6eab325da814b69e64
VictorManuelGracia <victorgracia97@gmail.com>
2022-11-25 14:40:57 +0100

    Changing the necessary ingredients depending on time_varying

--- types/+laxMPC/compute_laxMPC_ADMM_ingredients.m
+++ types/+laxMPC/compute_laxMPC_ADMM_ingredients.m
@@ -87,9 +87,11 @@ function vars = compute_laxMPC_ADMM_ingredients(controller, options, spcies_opti
     Aeq = [B -eye(n) zeros(n, size(Aeq, 2) - n); zeros(size(Aeq, 1), m) Aeq]; % Initial condition
     
     %% Compute matrix W
-    Hinv = inv(Hhat);
-    W = Aeq*Hinv*Aeq';
-    Wc = chol(W);
+    if ~options.time_varying
+        Hinv = inv(Hhat);
+        W = Aeq*Hinv*Aeq';
+        Wc = chol(W);
+    end
     
     %% Compute the tightened constraints
     if isa(controller, 'LaxMPC')
@@ -104,15 +106,17 @@ function vars = compute_laxMPC_ADMM_ingredients(controller, options, spcies_opti
     vars.n = n;
     vars.m = m;
     vars.N = N;
-    vars.Hi_0 = diag(Hinv(1:m, 1:m));
-    vars.Hi = reshape(diag(Hinv(m+(1:(N-1)*(n+m)),m+(1:(N-1)*(n+m)))), n+m, N-1)';
-    vars.Hi_N = Hinv(end-n+1:end, end-n+1:end);
-    vars.AB = [A B];
+    if ~options.time_varying
+        vars.Hi_0 = diag(Hinv(1:m, 1:m));
+        vars.Hi = reshape(diag(Hinv(m+(1:(N-1)*(n+m)),m+(1:(N-1)*(n+m)))), n+m, N-1)';
+        vars.Hi_N = Hinv(end-n+1:end, end-n+1:end);
+        vars.AB = [A B];
+        vars.Q = -diag(Q);
+        vars.R = -diag(R);
+        vars.T = -T;
+    end
     vars.UB = UB;
     vars.LB = LB;
-    vars.Q = -diag(Q);
-    vars.R = -diag(R);
-    vars.T = -T;
     
     % rho
     if (vars.rho_is_scalar)
@@ -163,9 +167,9 @@ function vars = compute_laxMPC_ADMM_ingredients(controller, options, spcies_opti
     end
     
     % Alpha and Beta
-        vars.Beta = zeros(n,n,N);
-        vars.Alpha = zeros(n,n,N-1);
         if ~options.time_varying
+            vars.Alpha = zeros(n,n,N-1);
+            vars.Beta = zeros(n,n,N);
             for i = 1:N
                 vars.Beta(:,:,i) = Wc((i-1)*n+(1:n),(i-1)*n+(1:n));
                 for j = 1:n
