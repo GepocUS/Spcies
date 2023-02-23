1857ed908d98c185764c30c68fe5e9bcb42ab558
VictorManuelGracia <victorgracia97@gmail.com>
2023-02-23 16:37:13 +0100

    Fixing error in header of lax_ADMM

--- types/+laxMPC/code_laxMPC_ADMM_C.c
+++ types/+laxMPC/code_laxMPC_ADMM_C.c
@@ -40,10 +40,10 @@ void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, dou
 #else
     
 #if time_varying == 0
-void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, double *u_opt, int *pointer_k, int *e_flag, solution *sol, double *update_time, double *solve_time, double *polish_time, double *run_time){
+void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, double *u_opt, int *pointer_k, int *e_flag, solution *sol){
 #else
 // void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, double *pointer_A, double *pointer_B, double *pointer_Q, double *pointer_R, double *pointer_T, double *u_opt, int *pointer_k, int *e_flag, solution *sol){
-void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, double *pointer_A, double *pointer_B, double *pointer_Q, double *pointer_R, double *u_opt, int *pointer_k, int *e_flag, solution *sol, double *update_time, double *solve_time, double *polish_time, double *run_time){
+void laxMPC_ADMM(double *pointer_x0, double *pointer_xr, double *pointer_ur, double *pointer_A, double *pointer_B, double *pointer_Q, double *pointer_R, double *u_opt, int *pointer_k, int *e_flag, solution *sol){
 #endif
 
 #endif
