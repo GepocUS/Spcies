d902111b079eed1a72b561fb6d2b3f476dc0c0c1
VictorManuelGracia <victorgracia97@gmail.com>
2023-05-03 11:58:39 +0200

    avoiding ingredients LB and UB as they are inputs now

--- types/+equMPC/cons_equMPC_ADMM_C.m
+++ types/+equMPC/cons_equMPC_ADMM_C.m
@@ -96,17 +96,17 @@ function constructor = cons_equMPC_ADMM_C(recipe)
 
     % Constants
     constCell = [];
-    if size(vars.LB, 2) > 1
-        % Different constraints for each prediction step
-        constCell = add_line(constCell, 'LB0', vars.LB(n+1:end, 1), 1, precision, var_options);
-        constCell = add_line(constCell, 'UB0', vars.UB(n+1:end, 1), 1, precision, var_options);
-        constCell = add_line(constCell, 'LB', vars.LB(:, 2:end)', 1, precision, var_options);
-        constCell = add_line(constCell, 'UB', vars.UB(:, 2:end)', 1, precision, var_options);
-        defCell = add_line(defCell, 'VAR_BOUNDS', 1, 1, 'int', 'define');
-    else
-        constCell = add_line(constCell, 'LB', vars.LB, 1, precision, var_options);
-        constCell = add_line(constCell, 'UB', vars.UB, 1, precision, var_options);
-    end
+%     if size(vars.LB, 2) > 1
+%         % Different constraints for each prediction step
+%         constCell = add_line(constCell, 'LB0', vars.LB(n+1:end, 1), 1, precision, var_options);
+%         constCell = add_line(constCell, 'UB0', vars.UB(n+1:end, 1), 1, precision, var_options);
+%         constCell = add_line(constCell, 'LB', vars.LB(:, 2:end)', 1, precision, var_options);
+%         constCell = add_line(constCell, 'UB', vars.UB(:, 2:end)', 1, precision, var_options);
+%         defCell = add_line(defCell, 'VAR_BOUNDS', 1, 1, 'int', 'define');
+%     else
+%         constCell = add_line(constCell, 'LB', vars.LB, 1, precision, var_options);
+%         constCell = add_line(constCell, 'UB', vars.UB, 1, precision, var_options);
+%     end
     if ~solver_options.time_varying
         constCell = add_line(constCell, 'Hi', vars.Hi, 1, precision, var_options);
         constCell = add_line(constCell, 'Hi_0', vars.Hi_0, 1, precision, var_options);
@@ -116,6 +116,7 @@ function constructor = cons_equMPC_ADMM_C(recipe)
         constCell = add_line(constCell, 'Alpha', vars.Alpha, 1, precision, var_options);
         constCell = add_line(constCell, 'Beta', vars.Beta, 1, precision, var_options);
     end
+    constCell = add_line(constCell, 'dummy', 5, 1, precision, var_options);
     if solver_options.in_engineering
         constCell = add_line(constCell, 'scaling_x', vars.scaling_x, 1, precision, var_options);
         constCell = add_line(constCell, 'scaling_u', vars.scaling_u, 1, precision, var_options);
